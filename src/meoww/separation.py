e874d2b0f5c2e4db0ad5f2c447e2dd9e6c0118a7
vandyG <vandy.goel23@gmail.com>
2024-10-25 18:06:46 -0500

    feat: add logic for source separation

---
+++ src/meoww/separation.py
@@ -0,0 +1,36 @@
+"""TODO: Docstring."""
+
+from pathlib import Path
+import demucs.separate
+
+
+def separate(path: Path, out: Path, shifts: int = 10):
+    """Separate vocals from an audio file using Demucs.
+
+    Args:
+        path (Path): Path to the audio file.
+        out (Path): Path to the output directory.
+        shifts (int, optional): Number of shifts for the Demucs algorithm. Defaults to 10.
+
+    Raises:
+        FileNotFoundError: If the audio file specified by `path` does not exist.
+        FileNotFoundError: If the output directory specified by `out` does not exist.
+    """
+    if not path.exists():
+        raise FileNotFoundError(f"File {path} does not exist.")
+
+    if not out.exists():
+        raise FileNotFoundError(f"Directory {out} does not exist.")
+
+    demucs.separate.main(
+        [
+            path,
+            "-o",
+            out,
+            "--shifts",
+            shifts,
+            "--two-stems",
+            "vocals",
+            "--flac",
+        ]
+    )
